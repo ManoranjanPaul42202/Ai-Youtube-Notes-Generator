bb71efd3ce4e6234d6183a2d06d38a1149de43da
ManoranjanPaul42202 <manoranjanpaul42202@gmail.com>
2026-03-02 21:23:33 +0530

    updates

--- tests/test_login.py
+++ tests/test_login.py
@@ -88,9 +88,13 @@ def driver():
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")  # VERY IMPORTANT
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+
+    driver.set_window_size(1920, 1080)  # Extra safety
+
     yield driver
     driver.quit()
 
